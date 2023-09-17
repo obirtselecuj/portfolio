00cddc39e9856e7bcc6aa4b2295310cfd9418ace
JucelEstribo <jucelestribo.dev@gmail.com>
2023-09-17 23:30:54 +0800

    update footer

--- src/components/footer/FooterBottom.js
+++ src/components/footer/FooterBottom.js
@@ -1,16 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const FooterBottom = () => {
-    const [currentYear, setCurrentYear] = useState(null);
-    useEffect(() => {
-        const currentDate = new Date();
-        const fullyear = currentDate.getFullYear();
-        setCurrentYear(fullyear);
-    })
     return (
         <div className="w-full py-10">
             <p className="text-center text-gray-500 text-base">
-                © {currentYear}. All rights reserved by Jucel Estribo
+                © All rights reserved by Jucel Estribo
             </p>
         </div>
     );
